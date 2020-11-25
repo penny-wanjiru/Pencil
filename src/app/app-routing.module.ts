48b7dc28a3db66c4448f5db593937d37e720f067
Penina Wanjiru <penina.wanjiru@andela.com>
2020-11-25 13:17:36 +0300

    Generate editor component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { EditorComponent } from '.editor/editor.component';
 
 const routes: Routes = [];
 
